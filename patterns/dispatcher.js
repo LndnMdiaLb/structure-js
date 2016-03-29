f6e1dc02a4e57a70046f80d22628de99f6413214
AndreasDilaveris <control@turnoffthe.tv>
2016-03-29 12:08:10 +0100

    add notes to Dispatcher.js

--- patterns/dispatcher.js
+++ patterns/dispatcher.js
@@ -1,27 +1,36 @@
 
 (function(app) {
 
-    //////////////////////////////////////////////////////////////////
-    //
-    // Dispatcher as a class
-    // with an encapsulated list of listeners available to all Instances
-    //
-    //////////////////////////////////////////////////////////////////
-
-
     app.Dispatcher = (function(){
 
+        //////////////////////////////////////////////////////////////////
+        //
+        // Dispatcher as a class
+        // with an encapsulated list of listeners available to all Instances
+        //
+        //////////////////////////////////////////////////////////////////
+
+        // All instances of these classes share / update the same vars
         var GLOBALCHANNELS={};
 
         // Dispatcher contsructor
-        function EventDispatcher(_private){
+        function Dispatcher(_private){
 
-            var _g = GLOBALCHANNELS
-            var CHANNELS = (_private)? {} : _g ;
+            //////////////////////
+            // private channel
+            //////////////////////
+
+            var
+                // if private create private CHANNELS OBJECT else reference GLOBALCHANNELS
+                CHANNELS = (_private)? {} : GLOBALCHANNELS ;
 
             this.register = function(ev, callback) {
                 if (typeof CHANNELS[ev] == 'undefined') CHANNELS[ev] = [];
                 CHANNELS[ev].push(callback);
+                /*
+                //
+                ( CHANNELS[ev] || ( CHANNELS[ev] = [] ) ) && CHANNELS[ev].push(callback) ;
+                */
             };
 
             this.unregister = function(ev, callback) {
@@ -30,27 +39,35 @@
                         if (CHANNELS[ev][s] == callback) CHANNELS[ev].splice(s, 1) ;
 
                 if(!CHANNELS[ev].length) delete CHANNELS[ev] ;
+
+                /*  // using indexOf() does not work in < ie9
+
+                var idx, c = CHANNELS ;
+                if ( c.hasOwnProperty(ev) && (idx = c[ev].indexOf(callback) ) != -1 ) c[ev].splice(idx, 1) ;
+                if ( !c[ev].length ) delete c[ev] ;
+                */
             };
 
             this.emit = function(ev, params, toglobal) {
 
                 // toglobal gives the opportunity to communicate with global observers from a private observer
-                var channels = (toglobal)? _g : CHANNELS ;
+                // if toglobal == true reference GLOBALCHANNELS during lifetime of method invokation
+                var channels = (toglobal)? GLOBALCHANNELS : CHANNELS ;
 
                 if (typeof channels[ev] != 'undefined')
                     for (var i = 0, l = channels[ev].length; i < l; i++)
                         channels[ev][i].call(this, ev, params);
 
+                // if instance is _private and toglobal == true also emit to private channels (by omitting togloal param)
                 if (toglobal && _private) this.emit(ev, params) ;
             };
 
         };
 
-        return EventDispatcher ;
+        return Dispatcher ;
 
     })();
 
 
 
 })(window.app || (window.app = {}));
-
