62f99c7b281b90093433931bde911c1ee0bbb1b8
Andreas Dilaveris <andreasdilaveris@Andreass-MacBook-Pro.local>
2016-10-14 10:17:26 +0100

    exernal.js : add IMGLoader class

--- utils/external.js
+++ utils/external.js
@@ -1,18 +1,106 @@
+  // dependency : dispatcher.js
+  // dependency : collections.js
+
 (function(app) {
 
     var utils = app.utils = app.utils || {};
 
-    // utils.loadDomImage = function(id, url, handler) {
-    //     var im = new Image();
-    //     var d = document.getElementById(id);
-    //     im.onload = function() {
-    //         d.appendChild(im);
-    //         var myimg = document.getElementsByTagName('img')[0];
-    //         handler(myimg);
-    //     };
-    //     im.src = url;
-    //     im.id = "";
-    // };
+
+        // current usage:
+        //     immediate
+        //         load url
+        //             emit global 'event' on completion
+        //             emit private 'event' on completion
+        //         load array of urls
+        //             emit global 'event' on completion
+        //             emit private 'event' on completion
+
+        //     pause
+        //         load url stored in key(s) .start()
+        //             emit global 'event' on completion
+        //             emit private 'event' on completion
+        //         load url stored in all key .start()
+        //             emit global 'event' on completion
+        //             emit private 'event' on completion
+
+
+
+    utils.IMGLoader = function IMGLoader (paused, _private){
+
+        var loading = new app.Map ,
+            dispatch = new app.Dispatcher(_private) ,
+            paused = paused || false,
+            defaultKey = 'images-loaded'
+            ;
+
+        // fast method to provide access to dispatcher api via composition
+        dispatch.through(this) ;
+
+        Object.defineProperty(this, 'DEFAULT', {
+            get:function(){
+                return defaultKey ;
+            }
+        }) ;
+        
+        // method to block immediate load of images
+        this.pause = function(){
+            paused = true
+        } ;
+
+        //  load image(s)
+        this.load = function(url, callback, emit) {
+
+            var key = emit || defaultKey
+
+            function createimg(url){
+                var im = new Image() ;
+                loading.add(key, im) ;
+                im.onload = function(e) {
+                    if(callback) callback.call(app, e) ;
+                    loading.remove(key, this) ;
+                    // if emit string included and 
+                    if(!loading[key]) dispatch.emit(key) ;
+                };
+
+                (paused) ? 
+                    im._src = url :
+                    im.src = url ;         
+            }
+
+            (url instanceof Array)?
+                url.forEach(createimg) :
+                createimg(url) 
+                ;
+
+            return dispatch ;
+        }
+
+        // remove
+        this.start = function(){
+
+            function src (im){
+                im.src = im._src ;
+                delete im._src ;
+            }
+
+            if (!arguments.length)
+                for (key in loading)
+                    loading[key].forEach(src)                
+
+            if (!!arguments.length)
+                Array.prototype.slice.call(arguments)
+                    .forEach( function(key){
+                        if(loading[key]) loading[key].forEach(src)
+                    })
+        }
+    }
+
+
+    /*
+    create img
+    on load
+    */
+
 
     // Is or isn't localhost.
 
